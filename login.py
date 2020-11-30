dfcacadf8065230712c4307f3980187e03b83b9d
Alexander <yspresi@yandex.ru>
2020-11-30 11:14:37 +0300

    added another two methods for check login

--- login.py
+++ login.py
@@ -10,25 +10,66 @@ class LoginCheck:
             return 'Вы ввели слишком длинный логин'
         return True
 
+    def _split_login(self, login):
+        """Делит логин на 3 части, так как разная логика проверки."""
+        first = login[0]
+        middle = login[1:-1]
+        last = login[-1] if len(login) > 1 else []
+        return first, middle, last
+
     def check_with_built_in(self, login):
-        status = self._check_length(login)
-        if status is True:
-            return status
-        return status
-
-    def check_with_prepared_sets(self, login):
-        status = self._check_length(login)
-        if status is True:
-            return status
-        return status
-
-    def check_with_regular_exp(self, login):
+        """Проверяет логин с помощью build-in методов python."""
         status = self._check_length(login)
         if status is True:
             start = time.time()
-            pattern = ('[a-zA-Z]' if len(login) == 1
-                       else '[a-zA-Z]+[0-9a-zA-Z]' if len(login) == 2
-                       else '[a-zA-Z]+[0-9a-zA-Z.-]+[0-9a-zA-Z]')
+            first, middle, last = self._split_login(login)
+            if (
+                first.isalpha()
+                and (not middle
+                     or middle.isalpha()
+                     or middle.isdecimal()
+                     or {'.'}.issubset(middle)
+                     or {'-'}.issubset(middle))
+                and (not last or last.isalpha() or last.isdecimal())
+            ):
+                end = time.time()
+                return f'Логин корректный. Время проверки {end-start}'
+            end = time.time()
+            return f'Логин НЕ корректный. Время проверки {end-start}'
+        return status
+
+    def check_with_prepared_sets(self, login):
+        """Проверяет логин сравнивая с подготовленными выборками."""
+        status = self._check_length(login)
+        if status is True:
+            start = time.time()
+            first, middle, last = self._split_login(login)
+            set_for_first = set('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')  # noqa
+            set_for_middle = set('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890.-')  # noqa
+            set_for_last = set('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890')  # noqa
+            if (
+                set(first).issubset(set_for_first)
+                and set(middle).issubset(set_for_middle)
+                and set(last).issubset(set_for_last)
+            ):
+                end = time.time()
+                return f'Логин корректный. Время проверки {end-start}'
+            end = time.time()
+            return f'Логин НЕ корректный. Время проверки {end-start}'
+        return status
+
+    def check_with_regular_exp(self, login):
+        """Проверяет логин с помощью регулярных выражений."""
+        status = self._check_length(login)
+        if status is True:
+            start = time.time()
+            pattern = (
+                '[a-zA-Z]'
+                if len(login) == 1
+                else '[a-zA-Z]+[0-9a-zA-Z]'
+                if len(login) == 2
+                else '[a-zA-Z]+[0-9a-zA-Z.-]+[0-9a-zA-Z]'
+            )
             if re.fullmatch(pattern, login):
                 end = time.time()
                 return f'Логин корректный. Время проверки {end-start}'
--- tests/test_login.py
+++ tests/test_login.py
@@ -22,6 +22,38 @@ def test_login_lenght(login_check):
 
 
 @mock.patch('time.time', mock.MagicMock(return_value=12345))
+def test_check_with_built_in(login_check):
+    answer = login_check.check_with_built_in
+    assert answer('a') == 'Логин корректный. Время проверки 0'
+    assert answer('a1') == 'Логин корректный. Время проверки 0'
+    assert (answer('1a') == 'Логин НЕ корректный. Время проверки 0'
+            ), 'Первый символ - латинская буква'
+    assert answer('a1a') == 'Логин корректный. Время проверки 0'
+    assert answer('a1.-a') == 'Логин корректный. Время проверки 0'
+    assert (answer('a1.-a.') == 'Логин НЕ корректный. Время проверки 0'
+            ), 'Последний символ - латинская буква или цифра'
+    assert (answer('a1@.-a.') == 'Логин НЕ корректный. Время проверки 0'
+            ), 'из не букв и не цифр допускаются точка и минус'
+    assert answer('Aa1.-a.A') == 'Логин корректный. Время проверки 0'
+
+
+@mock.patch('time.time', mock.MagicMock(return_value=12345))
+def test_check_with_prepared_sets(login_check):
+    answer = login_check.check_with_prepared_sets
+    assert answer('a') == 'Логин корректный. Время проверки 0'
+    assert answer('a1') == 'Логин корректный. Время проверки 0'
+    assert (answer('1a') == 'Логин НЕ корректный. Время проверки 0'
+            ), 'Первый символ - латинская буква'
+    assert answer('a1a') == 'Логин корректный. Время проверки 0'
+    assert answer('a1.-a') == 'Логин корректный. Время проверки 0'
+    assert (answer('a1.-a.') == 'Логин НЕ корректный. Время проверки 0'
+            ), 'Последний символ - латинская буква или цифра'
+    assert (answer('a1@.-a.') == 'Логин НЕ корректный. Время проверки 0'
+            ), 'из не букв и не цифр допускаются точка и минус'
+    assert answer('Aa1.-a.A') == 'Логин корректный. Время проверки 0'
+
+
+@mock.patch('time.time', mock.MagicMock(return_value=12345))
 def test_check_with_regular_exp(login_check):
     answer = login_check.check_with_regular_exp
     assert answer('a') == 'Логин корректный. Время проверки 0'
